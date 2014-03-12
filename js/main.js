ddef62532ec4f564de89c25ca01ddcf7b56ca455
brenwarren <brenwarren@me.com>
2014-03-12 16:09:53 +0100

    initial commit of site for php version

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+// map popup 
+
+//   $('#map-popover').popover();
+
+//  THIS IS FOR THE SERVICE AREA POPOVER - "Not sure if your location is covered?"
+$('#service-area-popover').popover();
+
+// THIS IS FOR THE "More services" popover button in the Services section
+$('#woodburners-popover').popover();
+
+$('#heating-popover').popover();
+
+$('#plumbing-popover').popover();
+
---
+++ project-files/README.md
@@ -0,0 +1,7 @@
+dalewarrenandson
+================
+
+Dale Warren and Son website
+
+New complete redesign of the dalewarrenandson.com website.
+The old wordpress website is to be replaced by a more modern, responsive lightweight design.
